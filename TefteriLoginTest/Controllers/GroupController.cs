9bbe506c98f387bd03c708aaf3bc41eaf6e29af1
JohnMos <mosxolios_giannis@hotmail.com>
2018-01-27 22:09:36 +0200

    Edit Group controller

--- TefteriLoginTest/Controllers/GroupController.cs
+++ TefteriLoginTest/Controllers/GroupController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TefteriLoginTest.Repository;
+using TefteriLoginTest.Models;
 
 namespace TefteriLoginTest.Controllers
 {
     public class GroupController : Controller
     {
+        private readonly FriendRepository friendRepository;
+
+        public GroupController(FriendRepository friendRepository)
+        {
+            this.friendRepository = friendRepository;
+        }
 
         // GET: Group
         public ActionResult CreateGroup()
         {
-            return View("CreateGroup");
+            List<Friend> friends = friendRepository.getAllFriends();
+            return View("CreateGroup", friends);
+        }
+
+        public ActionResult EditGroup()
+        {
+            return View("EditGroup");
         }
     }
 }
